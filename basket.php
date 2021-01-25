2f22894b2454e3f1f349503e87d30593012eedb4
Piotr Stanny <piotr.stanny@gmail.com>
2021-01-25 18:22:17 +0000

    Create basket session - check with Francois

--- basket.php
+++ basket.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include ("db.php");
 
 $pagename="Smart Basket"; //Create and populate a variable called $pagename
@@ -8,12 +9,23 @@ echo "<title>".$pagename."</title>"; //display name of the page as window title
 echo "<body>";
 include ("headfile.html"); //include header layout file
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
-$newprodid = $_POST['h_prodid'];
-$reququantity = $_POST['p_quantity'];
+
+if (isset($_POST['h_prodid'])) {
+  $newprodid = $_POST['h_prodid'];
+  $reququantity = $_POST['p_quantity'];
 
 
-echo $newprodid;
-echo $reququantity;
+  echo $newprodid;
+  echo $reququantity;
+
+  //create a new cell in the basket session array. Index this cell with the new product id.
+  //Inside the cell store the required product quantity
+  $_SESSION['basket'][$newprodid]=$reququantity;
+  echo "<p>1 item added";
+} else {
+  echo "<p>Basket unchanged";
+};
+
 
 
 include ("footfile.html"); //include head layout
