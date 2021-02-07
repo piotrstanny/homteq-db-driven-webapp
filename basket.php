ec040ef1f2e23f1ead89efb075a26376642f4d3d
Piotr Stanny <piotr.stanny@gmail.com>
2021-02-07 10:08:33 +0000

    Cosmetic improvement; remove "basket unchanged" msg

--- basket.php
+++ basket.php
@@ -10,16 +10,26 @@ echo "<body>";
 include ("headfile.html"); //include header layout file
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
 
+
+// This checks whether there is a product selected to be removed.
+if (isset($_POST['todelete_prodid'])) {
+  //Remove item from the SESSION array
+  $delprodid = $_POST['todelete_prodid'];
+  unset($_SESSION['basket'][$delprodid]);
+  echo "<p>1 item removed from the basket</p>";
+};
+
+
+// This checks whether there is a product to be added to the session, therefore to the basket.
 if (isset($_POST['h_prodid'])) {
   $newprodid = $_POST['h_prodid'];
   $reququantity = $_POST['p_quantity'];
-
   //create a new cell in the basket session array. Index this cell with the new product id.
   //Inside the cell store the required product quantity
   $_SESSION['basket'][$newprodid]=$reququantity;
-  echo "<p><strong>1 item added</strong></p><br>";
+  echo "<p><strong>1 item added</strong></p>";
 } else {
-  echo "<p><strong>Current basket unchanged</strong></p><br>";
+  echo "<p><strong>Current basket unchanged</strong></p>";
 };
 
 echo "<table id='baskettable'>";
@@ -28,6 +38,7 @@ echo "<tr>
     <th>Price</th>
     <th>Quantity</th>
     <th>Subtotal</th>
+    <th>Remove Item</th>
     </tr>";
 
 $total = 0;
@@ -40,10 +51,15 @@ if (isset($_SESSION['basket'])) {
 
     echo "<tr>
     <td>".$arrayp['prodName']."</td>
-    <td>&pound".$arrayp['prodPrice']."</td>
+    <td>&pound".number_format($arrayp['prodPrice'],2)."</td>
     <td>".$value."</td>
-    <td>&pound".$subtotal."</td>
-    </tr>";
+    <td>&pound".number_format($subtotal, 2)."</td>
+    <td>
+    <form action=basket.php method=POST>";
+    echo "<input type=submit name='submitbtn' value='REMOVE' id='submitbtn'>";
+    echo "<input type=hidden name=todelete_prodid value=".$arrayp['prodId'].">";
+    echo "</form>";
+    echo "</tr>";
     $total += $subtotal;
   }
 } else {
@@ -52,11 +68,16 @@ if (isset($_SESSION['basket'])) {
 
 echo "<tr>
     <th colspan=\"3\" style='text-align: right'>TOTAL:</th>
-    <th>&pound".$total."</th>
+    <th>&pound".number_format($total, 2)."</th>
+    <th></th>
+
     </tr>";
 
 echo "</table>";
-echo "<a href=clearbasket.php>CLEAR BASKET</a>";
+echo "<p><a href=clearbasket.php>CLEAR BASKET</a></p>";
+
+echo "<p>New Homteq customers: <a href=signup.php>Sign Up</a></p>";
+echo "<p>Returning Homteq customers: <a href=login.php>Log In</a></p>";
 
 include ("footfile.html"); //include head layout
 echo "</body>";
