2885f76a514a46294473679a9610b4e45d2bde89
Piotr Stanny <piotr.stanny@gmail.com>
2021-03-04 12:14:03 +0000

    Create form for login

--- login.php
+++ login.php
@@ -11,9 +11,25 @@ include ("headfile.html"); //include header layout file
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
 
 
-// Form to log in user
+//display a sign in form
+echo "<form action=login_process.php method=POST>";
+echo "<table id='baskettable'>";
 
+  echo "<tr>";
+    echo "<td>*Email</td>";
+    echo "<td><input type=text name=r_email size=40></td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>*Password</td>";
+    echo "<td><input type=password name=r_password1 size=40></td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td><input type=reset value='Clear Form' id='submitbtn'></td>";
+    echo "<td><input type=submit value='Sign In' id='submitbtn'></td>";
+  echo "</tr>";
 
+echo "</table>";
+echo "</form>";
 
 include ("footfile.html"); //include head layout
 echo "</body>";
